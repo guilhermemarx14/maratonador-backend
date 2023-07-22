34b1d0384a24edbba8ae86755ba8a786ed587d2d
Guilherme Marx Ferreira Tavares <guilhermemarx14@gmail.com>
2023-07-22 08:16:48 -0300

    initial commit

---
+++ src/main/java/com/guilhermemarx14/maratonadorbackend/MaratonadorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermemarx14.maratonadorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaratonadorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaratonadorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermemarx14/maratonadorbackend/MaratonadorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermemarx14.maratonadorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaratonadorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
